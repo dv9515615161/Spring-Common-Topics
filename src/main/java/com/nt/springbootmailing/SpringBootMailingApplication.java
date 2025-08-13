a53a1753a0dff4d401218a56e6d63fd8986cdd19
vijay <dv9515615161@gmail.com>
2025-08-13 11:43:51 +0530

    Initial commit

---
+++ src/main/java/com/nt/springbootmailing/SpringBootMailingApplication.java
@@ -0,0 +1,13 @@
+package com.nt.springbootmailing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMailingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMailingApplication.class, args);
+    }
+
+}
